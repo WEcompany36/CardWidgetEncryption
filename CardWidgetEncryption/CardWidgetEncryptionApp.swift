03083c86defaa76a930f4f59a2e6aa9295d7c400
William Gallegos <william.gallegos0306@gmail.com>
2025-01-23 23:40:10 -0700

    Initial Commit

---
+++ CardWidgetEncryption/CardWidgetEncryptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWidgetEncryptionApp.swift
+//  CardWidgetEncryption
+//
+//  Created by William Gallegos on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CardWidgetEncryptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
